e957803d9859230ac85823de29d1a23114ed1c7a
Josh H <jrhurley5333@gmail.com>
2022-06-13 22:12:28 -0400

    Begin solution for 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function () {
+  let string = "";
+  let num = 10;
 
+  for (i = 0; i < num; i++) {
+    return string;
+    if (i < num) {
+      string += "hey";
+    }
+  }
+  return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
