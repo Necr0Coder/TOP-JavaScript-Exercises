6266de4181e0f9309fd109ebb534fbce657addc0
Necr0Coder <53581330+Necr0Coder@users.noreply.github.com>
2022-06-27 16:09:23 -0400

    first guess.  test with NPM

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(...nums) {
+// arg + ...args = sumAll
+// if arg is not a number, return "error"
+//else return sumAll
+for (num in nums) {
+    let sumAll;
+    const posNumber = (nums >= 0);
+    if (num.typeOf === posNumber) {
+        sumAll =+
+    } else {
+        console.log("ERROR")
+        break;
+    }
+return sumAll;
+}
 };
 
 // Do not edit below this line
