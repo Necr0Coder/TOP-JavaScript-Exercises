8f1ba78663d6334acec9265e0583096c5f56c661
Necr0Coder <53581330+Necr0Coder@users.noreply.github.com>
2022-07-05 13:00:23 -0400

    First solution.  Need to test with NPM

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {           //subtract 32 and multiply by .5556 (or 5/9)
+ let newTemp = 0;
+  newTemp = .5556 * (temp - 32);
+  temp = newTemp.toFixed(1);
+  return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {       //multiply by 1.8 (or 9/5) and add 32
+  let newTemp = 0;
+  newTemp = temp * 1.8 + 32;
+  temp = newTemp.toFixed(1);
+  return temp;
 };
 
 // Do not edit below this line
