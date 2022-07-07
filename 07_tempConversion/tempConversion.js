470b2e2d1eb0617362d669eab44aadc52b96efb7
Josh H <jrhurley5333@gmail.com>
2022-07-06 20:40:56 -0400

    tested with NPM.  Submitting final.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,19 +1,23 @@
-const ftoc = function(temp) {           //subtract 32 and multiply by .5556 (or 5/9)
- let newTemp = 0;
-  newTemp = .5556 * (temp - 32);
-  temp = newTemp.toFixed(1);
+const ftoc = function (temp) {
+  //subtract 32 and multiply by .5556 (or 5/9)
+  let newTemp = 0;
+  newTemp = 0.5556 * (temp - 32);
+  //added "+" to return a Number. "toFixed" returns as string.
+  temp = +newTemp.toFixed(1);
   return temp;
 };
 
-const ctof = function(temp) {       //multiply by 1.8 (or 9/5) and add 32
+const ctof = function (temp) {
+  //multiply by 1.8 (or 9/5) and add 32
   let newTemp = 0;
   newTemp = temp * 1.8 + 32;
-  temp = newTemp.toFixed(1);
+  //added "+" to return a Number. "toFixed" returns as string.
+  temp = +newTemp.toFixed(1);
   return temp;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
