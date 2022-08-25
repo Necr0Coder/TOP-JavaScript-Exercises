369e18c8c47c2063af702122868b1dea800ab1b7
necr0coder <jrhurley5333@gmail.com>
2022-08-24 21:47:22 -0400

    completed palindromes.  need to finish fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  //make lowercase and replace spaces and special characters with nothing.
+  let newStr = str.toLowerCase().replace(/[^a-z]/g, "");
 
+  return newStr.split("").reverse().join("") == newStr;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+  // A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
 
+  let num;
+  let array = [];
+  let prevNum = array[(-2, -1)];
+
+  for (i = 0; i < position; i++) {
+    array.push((i += prevNum));
+  }
+  console.log(array);
+  return array.pop();
 };
 
 // Do not edit below this line
